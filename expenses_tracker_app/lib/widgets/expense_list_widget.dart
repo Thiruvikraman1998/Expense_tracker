13a094f282c101fc3d9aea1e4b9822764c0b2e64
Thiruvikraman1998 <development.thiru@gmail.com>
2023-05-26 18:37:20 +0530

    ordered List

--- expenses_tracker_app/lib/widgets/expense_list_widget.dart
+++ expenses_tracker_app/lib/widgets/expense_list_widget.dart
@@ -10,6 +10,7 @@ class ExpensesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    expenses.sort((b, a) => a.amount.compareTo(b.amount));
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (context, index) {
